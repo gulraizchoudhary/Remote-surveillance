ef3fb34b678d7bfc67029949ad2471222c1f0c88
G. I. Choudhary <30568161+coding-guruz@users.noreply.github.com>
2022-05-03 17:40:58 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,17 +4,19 @@
 #include <thread> 
 
 void connect(){
+	//Login to PTZ Camera
 	Camera cam;
 	cam.setLoginDetails("10.243.29.34", 8000, "admin", "test123");
 	cam.login();
 	
+	//Write the videos of the location where the threat is detected.
 	cam.WriteAVIvideo();
 }
 
 int main() {
 	
-	//connect();
-
+	connect();
+	//Predict the threat based on the trained model. 
 	HOG hog;
 /*	
 	hog.createXmlDescriptor("./pos/img","Positive.xml",1000);
